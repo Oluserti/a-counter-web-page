97809be34ff7bc0a3bc3acba94853a511b10e258
Oluserti <alliolumide0@gmail.com>
2021-07-10 17:19:07 +0100

    Added file to the git

---
+++ script.js
@@ -0,0 +1,33 @@
+// let decreaseBtn = document.getElementById("button_decrease");
+// let increaseBtn = document.getElementById("button_increase")
+// let counter = document.getElementById("ounter")
+
+// decreaseBtn.addEventListener("click", ()=>{
+//     console.log('decrease');
+// })
+
+// let example = 'true'
+// console.log(example);
+
+
+
+// document.getElementById("button-increase").addEventListener('click', function myFunction(){
+//     let counter = parseInt(document.getElementById('counter').innerHTML)
+//     if (value >= 50){
+//         let newCounter = counter
+//         document.getElementById("counter").innerHTML = newCounter
+//     }else{
+//         let newCounter = counter +1
+//         document.getElementById("counter").innerHTML = newCounter
+//     }
+// })
+// document.getElementById("button-decrease").addEventListener('click', function myFunction(){
+//     let counter = parseInt(document.getElementById('counter').innerHTML)
+//     if(value ==0){
+//         let newCounter = counter
+//         document.getElementById("counter").innerHTML = newCounter
+//     }else{
+//         let newCounter = counter - 1
+//         document.getElementById("counter").innerHTML = newCounter
+//     }
+// })
