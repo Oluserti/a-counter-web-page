54154889dbe63ae758bcbebc6a05518984b69d5e
Oluserti <alliolumide0@gmail.com>
2021-07-11 05:38:42 +0100

    Linked javascript with tthe code

--- script.js
+++ script.js
@@ -1,33 +1,20 @@
-// let decreaseBtn = document.getElementById("button_decrease");
-// let increaseBtn = document.getElementById("button_increase")
-// let counter = document.getElementById("ounter")
-
-// decreaseBtn.addEventListener("click", ()=>{
-//     console.log('decrease');
-// })
-
-// let example = 'true'
-// console.log(example);
-
-
-
-// document.getElementById("button-increase").addEventListener('click', function myFunction(){
-//     let counter = parseInt(document.getElementById('counter').innerHTML)
-//     if (value >= 50){
-//         let newCounter = counter
-//         document.getElementById("counter").innerHTML = newCounter
-//     }else{
-//         let newCounter = counter +1
-//         document.getElementById("counter").innerHTML = newCounter
-//     }
-// })
-// document.getElementById("button-decrease").addEventListener('click', function myFunction(){
-//     let counter = parseInt(document.getElementById('counter').innerHTML)
-//     if(value ==0){
-//         let newCounter = counter
-//         document.getElementById("counter").innerHTML = newCounter
-//     }else{
-//         let newCounter = counter - 1
-//         document.getElementById("counter").innerHTML = newCounter
-//     }
-// })
+document.getElementById('button-increase').addEventListener('click', function myFunction(){
+    var value = parseInt(document.getElementById('value').innerHTML)
+    if (value >= 50){
+        var newValue = value
+        document.getElementById('value').innerHTML = newValue
+    }else{
+        var newValue = value + 1
+        document.getElementById('value').innerHTML = newValue
+    }
+})
+document.getElementById('button-decrease').addEventListener('click', function myFunction(){
+    var value = parseInt(document.getElementById('value').innerHTML)
+    if(value <=0){
+        var newValue = value
+        document.getElementById('value').innerHTML = newValue
+    }else{
+        var newValue = value - 1
+        document.getElementById('value').innerHTML = newValue
+    }
+})
